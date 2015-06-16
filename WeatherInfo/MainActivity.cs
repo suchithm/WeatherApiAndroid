1e652aa7bf6363ef8a5f9a183209a6f0e141822e
suchithm <suchimadavu@gmail.com>
2015-06-16 18:24:32 +0530

    * WeatherInfo.csproj: * Main.axml: * Resource.designer.cs: * Main.axml:
    * MainActivity.cs: layout &bind progress

--- WeatherInfo/MainActivity.cs
+++ WeatherInfo/MainActivity.cs
@@ -8,6 +8,8 @@ using Android.OS;
 using Newtonsoft.Json;
 //http://l.yimg.com/a/i/us/we/52/28.gif
 
+//http://l.yimg.com/a/i/us/we/52/30.gif
+
 //select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="bangalore") and u='c'
 
 //https://query.yahooapis.com/v1/public/yql?q=qry&format=json
@@ -16,22 +18,33 @@ using Newtonsoft.Json;
 using System.Threading.Tasks;
 using System.Net;
 using Android.Views.InputMethods;
+using Android.Graphics;
 
 
 namespace WeatherInfo
 {
-	[Activity ( Label = "WeatherInfo" , MainLauncher = true , Icon = "@drawable/icon" )]
+	[Activity ( Label = "Weather Condition" , MainLauncher = true , Icon = "@drawable/icon" )]
 	public class MainActivity : Activity
 	{ 
+		ImageView imgCondition;
+		TextView lblTemp;
+		TextView lblHumidity;
+		TextView lblWindSpeed;
+		TextView lblConditionText;
+		TextView lblSunrise;
+		TextView lblSunSet;
+		ListView ListViewForeCast;
+		EditText txtLocation;
+		WeatherClass objYahooWeatherClass;
+		
 		const string strYahooApi=  "https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"bangalore\") and u='c'&format=json";
 		protected override void OnCreate ( Bundle bundle )
 		{
-			base.OnCreate ( bundle );
-			WeatherClass objYahooWeatherClass;
+			base.OnCreate ( bundle ); 
 			// Set our view from the "main" layout resource
 			SetContentView ( Resource.Layout.Main ); 
-			EditText txtLocation = FindViewById<EditText> ( Resource.Id.txtSearch );
-			
+			InitializeControl ();
+	
 			txtLocation.KeyPress +=async delegate(object sender, View.KeyEventArgs e) {
 				InputMethodManager inputManager = (InputMethodManager)this.GetSystemService (Context.InputMethodService); 
 				inputManager.HideSoftInputFromWindow (this.CurrentFocus.WindowToken, HideSoftInputFlags.NotAlways); 
@@ -48,10 +61,32 @@ namespace WeatherInfo
 						Console.WriteLine(" date "+ forecast.date);
 						Console.WriteLine(" day "+forecast.day);
 					}
+					BindData();
 				}
 			}; 
 				
 			 
+		}
+		void BindData()
+		{
+			lblTemp.Text = string.Format ( "Temperature :{0}{1}" , objYahooWeatherClass.query.results.channel.item.condition.temp , objYahooWeatherClass.query.results.channel.units.temperature );
+			lblHumidity.Text = string.Format ( "Humidity :{0}{1}" , objYahooWeatherClass.query.results.channel.atmosphere.humidity,"%" );
+			lblWindSpeed.Text=string.Format ( "Wind Speed :{0}{1}" , objYahooWeatherClass.query.results.channel.wind.speed,objYahooWeatherClass.query.results.channel.units.speed );
+			lblConditionText.Text=string.Format ( " {0}" , objYahooWeatherClass.query.results.channel.item.condition.text );
+			lblSunrise.Text =string.Format("Sunrise :{0}",objYahooWeatherClass.query.results.channel.astronomy.sunrise);
+			lblSunSet.Text =string.Format("Sunset :{0}",objYahooWeatherClass.query.results.channel.astronomy.sunset);
+	 
+		}
+		void InitializeControl()
+		{
+			imgCondition = FindViewById<ImageView> ( Resource.Id.imgWeatherIcon ); 
+			txtLocation = FindViewById<EditText> ( Resource.Id.txtSearch );
+			lblTemp = FindViewById<TextView> ( Resource.Id.lblTempWC );
+			lblHumidity=FindViewById<TextView> ( Resource.Id.lblHumidity );
+			lblWindSpeed=FindViewById<TextView> ( Resource.Id.lblWindSpeed );
+			lblConditionText=FindViewById<TextView> ( Resource.Id.lblConditionText );
+			lblSunrise=FindViewById<TextView> ( Resource.Id.lblSunrise );
+			lblSunSet=FindViewById<TextView> ( Resource.Id.lblSunset ); 
 		}
 		async Task<string> fnDownloadString(string strUri)
 		{ 
--- WeatherInfo/Resources/Resource.designer.cs
+++ WeatherInfo/Resources/Resource.designer.cs
@@ -60,26 +60,53 @@ namespace WeatherInfo
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050002
-			public const int imgWeatherIcon = 2131034114;
+			// aapt resource value: 0x7f050011
+			public const int ListViewForeCast = 2131034129;
 			
-			// aapt resource value: 0x7f050006
-			public const int lblDevider1RP = 2131034118;
+			// aapt resource value: 0x7f05000c
+			public const int imgWeatherIcon = 2131034124;
+			
+			// aapt resource value: 0x7f05000f
+			public const int lblConditionText = 2131034127;
+			
+			// aapt resource value: 0x7f050001
+			public const int lblCurrentLocation = 2131034113;
+			
+			// aapt resource value: 0x7f050010
+			public const int lblForeCastHeadingTxt = 2131034128;
 			
 			// aapt resource value: 0x7f050007
-			public const int lblDevider2RP = 2131034119;
+			public const int lblHumidity = 2131034119;
 			
-			// aapt resource value: 0x7f050004
-			public const int lblProductNameRP = 2131034116;
+			// aapt resource value: 0x7f050006
+			public const int lblLine1WC = 2131034118;
 			
 			// aapt resource value: 0x7f050008
-			public const int lblProductNameRP2 = 2131034120;
+			public const int lblLine2WC = 2131034120;
+			
+			// aapt resource value: 0x7f05000a
+			public const int lblLine3WC = 2131034122;
+			
+			// aapt resource value: 0x7f05000d
+			public const int lblSunrise = 2131034125;
+			
+			// aapt resource value: 0x7f05000e
+			public const int lblSunset = 2131034126;
+			
+			// aapt resource value: 0x7f050004
+			public const int lblTempWC = 2131034116;
+			
+			// aapt resource value: 0x7f05000b
+			public const int lblVisibility = 2131034123;
+			
+			// aapt resource value: 0x7f050009
+			public const int lblWindSpeed = 2131034121;
 			
 			// aapt resource value: 0x7f050003
 			public const int llWeatherDataTxt = 2131034115;
 			
-			// aapt resource value: 0x7f050001
-			public const int rlTopMainContainer = 2131034113;
+			// aapt resource value: 0x7f050002
+			public const int rlTopMainContainer = 2131034114;
 			
 			// aapt resource value: 0x7f050000
 			public const int txtSearch = 2131034112;
